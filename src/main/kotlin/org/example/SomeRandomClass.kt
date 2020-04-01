e6e385527d69bcf28b23d8c49e24f084c6358142
Emiliano Tebes <emiliano.tebes@despegar.com>
2020-04-01 19:04:19 -0300

    add dependencies

--- src/main/kotlin/org/example/SomeRandomClass.kt
+++ src/main/kotlin/org/example/SomeRandomClass.kt
@@ -2,8 +2,12 @@ package org.example
 
 import java.lang.NullPointerException
 
-class SomeRandomClass() {
-    fun someRandomFunction(a: String, b: String, c: String) {
-        throw NullPointerException("I throw a NPE")
+class SomeRandomClass {
+    fun someRandomFunction(a: String): String {
+        return otherFunction(a)
+    }
+
+    private fun otherFunction(a: String): String {
+        throw NullPointerException("I throw a NPE, you should see my stacktrace, not just my message.")
     }
 }
---
+++ src/test/kotlin/org/example/SomeRandomClassTest.kt
@@ -0,0 +1,28 @@
+package org.example
+
+import io.kotest.core.spec.style.WordSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+
+class StringSpecExample : WordSpec() {
+    private val myClass = SomeRandomClass()
+
+    init {
+
+        "this test should throw a NPE with stacktrace" should {
+
+            "but it loses stacktrace" {
+                forAll(
+                    row("a"),
+                    row("simple"),
+                    row("data test")
+                ) { a ->
+                    myClass.someRandomFunction(a)
+                    assert(true)
+                }
+            }
+        }
+
+    }
+
+}
